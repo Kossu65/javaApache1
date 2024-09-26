0f110d7d10bbd3653b0fe9f2094c1b95ee45f8f9
Kossu65 <kosonenmax@gmail.com>
2024-09-26 12:32:04 +0300

    Add files via upload

---
+++ personalinfo1.java
@@ -0,0 +1,28 @@
+public class Java1personalinfo {
+    public static void main(String[] args) {
+        // Step 1: Declare variables
+        String firstName;   // String
+        String lastName;    // String
+        int age;            // Integer
+        double height;      // Double (height in cm)
+        boolean isStudent;  // Boolean
+
+        // Step 2: Assign values to the variables
+        firstName = "Max";
+        lastName = "Kosonen";
+        age = 15;
+        height = 177;   // in centimeters
+        isStudent = true; // true or false
+
+        // Step 3: Output the values
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Age: " + age);
+        System.out.println("Height (cm): " + height);
+        System.out.println("Is a student: " + isStudent);
+    }
+}
+      
+        
+    
+
